19e9c10187eef7f0ee4ce135288b8c332313259a
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2026-09-09 02:37:09 +0000

    fixing up stuf

--- API/recorder.py
+++ API/recorder.py
@@ -8,7 +8,7 @@ class Recorder:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        self.val_loss_min = np.inf
         self.delta = delta
 
     def __call__(self, val_loss, model, path):
