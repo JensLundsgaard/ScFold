7f195358fa19007322656b5eaac0b045af194c9b
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2026-09-10 04:38:53 +0000

    Fixing reshaping stuff

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ class Exp:
                     # self._save(name=str(epoch))
                     #self.test()
                 
-                print(f" {sum(params.numel() for params in self.method.model.parameters() if params.requires_grad())} & ${valid_acc_1s.mean():.3f} \\pm {valid_acc_1s.std():.3f}$ & ${valid_acc_5s.mean():.3f} \\pm {valid_acc_5s.std():.3f}$ & ${valid_acc_10s.mean():.3f} \\pm {valid_acc_10s.std():.3f}$ & ${np.exp(valid_losses).mean():.3f} \\pm {np.exp(valid_losses).std():.3f}$ ")
+                print(f" {sum(params.numel() for params in self.method.model.parameters() if params.requires_grad)} & ${valid_acc_1s.mean():.3f} \\pm {valid_acc_1s.std():.3f}$ & ${valid_acc_5s.mean():.3f} \\pm {valid_acc_5s.std():.3f}$ & ${valid_acc_10s.mean():.3f} \\pm {valid_acc_10s.std():.3f}$ & ${np.exp(valid_losses).mean():.3f} \\pm {np.exp(valid_losses).std():.3f}$ ")
 
                 recorder(valid_loss, self.method.model, self.path)
                 if recorder.early_stop:
