2831a26500a1fe3aadfa70854183465f1af845b5
Jens Lundsgaard <jenslundsgaard7@gmail.com>
2026-09-22 18:45:41 +0000

    fixing up scfold

--- API/dataloader.py
+++ API/dataloader.py
@@ -10,27 +10,55 @@ import inspect
 from API.h5_dataset import H5Dataset
 from API.pdb_dataset import PDBDataset
 import itertools
+import zlib
+def pick_frame(num_frames, protein_id, seed, frame_index=None):
+    if frame_index is not None:
+        if not -num_frames <= frame_index < num_frames:
+            raise IndexError("frame_index {} out of range for {} ({} frames)".format(
+                frame_index, protein_id, num_frames))
+        return int(frame_index % num_frames)
+    rng = np.random.default_rng([int(seed), zlib.crc32(protein_id.encode())])
+    return int(rng.integers(num_frames))
 
 # I am editing this to insert my own Dataset into the dataloader
 def load_data(data_name, method, batch_size, data_root, num_workers=8, **kwargs):
     # mdcath_spinet_320_0.h5 
     # mdcath_spinet_450_0.h5 
     # atlas_data.h5
+    h5_path = "atlas_data.h5"
     use_pdbs = True
+    h5_path = osp.join("..",h5_path)
+    temp = 320
+    if(h5_path == "atlas_data.h5"):
+        split_df = pd.read_csv(osp.join("..", "atlas_cross_val_index.csv"))
+        val_mask = split_df["cross_val"] == 0 # change to whatever cross val sets you want
+        test_mask = split_df["cross_val"] == 4
 
-    h5_path = osp.join("..", "atlas_data.h5")
+        train_indices = split_df[(~val_mask) & (~test_mask)]["random_indices"].to_list()
+        val_indices = split_df[val_mask]["random_indices"].to_list()
+        test_indices = split_df[test_mask]["random_indices"].to_list()
+        
+        val_groups = split_df[val_mask]["pdb"].to_list()
+        train_groups = split_df[(~val_mask) & (~test_mask)]["pdb"].to_list()
+        test_groups = split_df[test_mask]["pdb"].to_list()
+    else:
+        h5_path = os.path.join("..", f"mdcath_spinet_{temp}_0.h5")
+        index = pd.read_csv(os.path.join("..",f"mdcath_{temp}_0_topology_split.csv"))
 
-    split_df = pd.read_csv(osp.join("..", "atlas_cross_val_index.csv"))
-    val_mask = split_df["cross_val"] == 0 # change to whatever cross val sets you want
-    test_mask = split_df["cross_val"] == 4
+        train_groups = index[index["split"] == "train"]["domain"].tolist()
+        test_groups = index[index["split"] == "test"]["domain"].tolist()
+        val_groups = index[index["split"] == "validation"]["domain"].tolist()        
+        pdb_to_size = {}
+        def visit(name, obj):
+            if isinstance(obj, h5py.Group) and "coordinates" in obj:
+                pdb_to_size[name.split("/")[0]] = obj["coordinates"].shape[1]
+        with h5py.File(h5_path, "r") as f:
+            f.visititems(visit)
 
-    train_indices = split_df[(~val_mask) & (~test_mask)]["random_indices"].to_list()
-    val_indices = split_df[val_mask]["random_indices"].to_list()
-    test_indices = split_df[test_mask]["random_indices"].to_list()
-    
-    val_groups = split_df[val_mask]["pdb"].to_list()
-    train_groups = split_df[(~val_mask) & (~test_mask)]["pdb"].to_list()
-    test_groups = split_df[test_mask]["pdb"].to_list()
+        train_indices = [pick_frame(pdb_to_size[pdb_id], pdb_id, 0) for pdb_id in train_groups]
+        val_indices = [pick_frame(pdb_to_size[pdb_id], pdb_id, 0) for pdb_id in val_groups]
+        test_indices = [pick_frame(pdb_to_size[pdb_id], pdb_id, 0) for pdb_id in test_groups]
+
     if not use_pdbs:
         train_set = H5Dataset(h5_path, random_indices=train_indices, groups=train_groups)
         valid_set = H5Dataset(h5_path, random_indices=val_indices, groups=val_groups)
